fd441a651097bf989b2966cbeca190ae8934f784
Brandon Mitchell <brandonmitchell217@gmail.com>
2021-03-06 00:21:44 -0500

    Added gsap & started tinkering with nav animation. Added a new JS file for gsap. Need to add a js file for each seperate page, too many console conflicts.

--- main1.js
+++ main1.js
@@ -1,1 +1,18 @@
+gsap.to(".navLogo", {
+  duration: 1,
+  ease: "power1.in",
+  x: 140,
+  rotation: 360,
+});
 
+gsap.to("#navList", {
+  duration: 3,
+  ease: "power1.in",
+  opacity: 1,
+});
+
+gsap.to("#navList li a", {
+  duration: 2.5,
+  ease: "power1.in",
+  textShadow: "0 2px 8px red",
+});
